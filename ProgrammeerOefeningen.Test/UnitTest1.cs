643455c037e800cde1f003c6acbf898fac966584
Bart Steyaert <Bart.Steyaert@realdolmen.com>
2021-02-07 10:39:52 +0100

    oefening 1

--- ProgrammeerOefeningen.Test/UnitTest1.cs
+++ ProgrammeerOefeningen.Test/UnitTest1.cs
@@ -7,5 +7,12 @@ namespace ProgrammeerOefeningen.Test
 {
     public class Test
     {
+        [Fact]
+        public void Oefening1()
+        {
+            var kat = Program.Oefening1();
+            kat.Naam.Should().Be("Josefien");
+            kat.ZegNaam().ToLowerInvariant().Should().Be("Hallo, ik ben Josefien.".ToLowerInvariant());
+        }
     }
 }
--- Programmeeroefeningen/Program.cs
+++ Programmeeroefeningen/Program.cs
@@ -6,8 +6,26 @@ namespace Programmeeroefeningen
     {
         public static void Main(string[] args)
         {
-
+            Oefening1();
         }
 
+        public static Kat Oefening1()
+        {
+            // maak een class Kat
+            var kat = new Kat();
+            // maak een methode in de class Kat die Spreek() noemt. De spreek methode schrijft "miauw!" in de console
+            kat.Spreek();
+            // geef de kat een Property naam en zorg dat het nieuwe kat-object de naam "Joske" krijgt
+
+            // zorg dat de kat "Hallo, ik ben Joske." teruggeeft in een methode GeefNaam()
+            // opgelet! niet in de console schrijven maar teruggeven als string zodat we die als variable kunnen opslaan.
+            string naam = kat.ZegNaam();
+
+            // verander nu de naam naar Josefien
+            // zorg dat de methode ZegNaam ook automatisch aanpast zodat die de juiste naam teruggeeft.
+            // !Tip: gebruik de juiste property
+
+            return kat;
+        }
     }
 }
