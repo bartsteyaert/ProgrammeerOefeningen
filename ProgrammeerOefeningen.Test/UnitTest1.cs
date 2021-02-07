670dbf8c95a72d2c9f497460d2d56e0b928698bf
Bart Steyaert <Bart.Steyaert@realdolmen.com>
2021-02-07 10:38:12 +0100

    Add project files.

---
+++ ProgrammeerOefeningen.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+using Programmeeroefeningen;
+using FluentAssertions;
+
+namespace ProgrammeerOefeningen.Test
+{
+    public class Test
+    {
+    }
+}
---
+++ Programmeeroefeningen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Programmeeroefeningen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
